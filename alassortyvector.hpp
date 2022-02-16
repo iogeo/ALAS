761dc4c37cd99f4af891e200b472fe2229cce32d
iogeo <99806509+iogeo@users.noreply.github.com>
2022-02-16 06:45:09 -0800

    Add files via upload

---
+++ alassortyvector.hpp
@@ -0,0 +1,113 @@
+#ifndef ALASSORTY
+#define ALASSORTY
+
+namespace alas
+{
+	template <typename t> class sortyvector
+	{
+		public:
+		t* array;
+		int size;
+		int maxsize;
+		sortyvector()
+		{
+			size=0;
+			maxsize=0;
+		}
+		~sortyvector()
+		{
+			delete[] array;
+		}
+		void push_back(t a)
+		{
+			if(size==maxsize)
+			{
+				if(size)
+				{
+					int* temparray=array;
+					array=new t[size*2];
+					for(int i=0; i<size; i++)
+					{
+						array[i]=temparray[i];
+					}
+					delete[] temparray;
+					maxsize=size*2;
+				}
+				else
+				{
+					array=new t[1];
+					maxsize=1;
+				}
+			}
+			bool b=0;
+			for(int i=0; i<size; i++)
+			{
+				if(!b)
+				{
+					if(array[i]>=a)
+					{
+						for(int j=size; j>=i; j--)
+						{
+							array[j]=array[j-1];
+						}
+						array[i]=a;
+						b=1;
+					}
+				}
+			}
+			if(!b)
+			{
+				array[size]=a;
+			}
+			size++;
+		}
+		void resize(int a)
+		{
+			if(maxsize)
+			{
+				int* temparray=array;
+				array=new t[a];
+				for(int i=0; i<size; i++)
+				{
+					array[i]=temparray[i];
+				}
+				delete[] temparray;
+				maxsize=a;
+			}
+			else
+			{
+				array=new t[a];
+				maxsize=a;
+			}
+		}
+		void erase(int a)
+		{
+			for(int j=a; j<size-1; j++)
+			{
+				array[j]=array[j+1];
+			}
+			size--;
+		}
+		void erase(int a, int c)
+		{
+			for(int j=a; j<size; j++)
+			{
+				array[j]=array[j+c];
+			}
+			size-=c;
+		}
+		void shrink_to_fit()
+		{
+			int* temparray=array;
+			array=new t[size];
+			for(int i=0; i<size; i++)
+			{
+				array[i]=temparray[i];
+			}
+			delete[] temparray;
+			maxsize=size;
+		}
+	};
+}
+
+#endif
